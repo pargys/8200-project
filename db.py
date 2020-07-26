20b967d12511aad9bbcd5f2256b5b7369263a1c8
pargys <ps@sem-wolf.org>
2020-07-26 23:58:07 +0300

    implementation of the create_table method

---
+++ db.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Dict, List, Type
+from dataclasses_json import dataclass_json
+DB_ROOT = Path('db_files')
+
+import shelve
+
+@dataclass_json
+@dataclass
+class DBField:
+    name: str
+    type: Type
+
+
+@dataclass_json
+@dataclass
+class SelectionCriteria:
+    field_name: str
+    operator: str
+    value: Any
+
+
+@dataclass_json
+@dataclass
+class DBTable:
+    name: str
+    fields: List[DBField]
+    key_field_name: str
+
+
+@dataclass_json
+@dataclass
+class DataBase:
+    # Put here any instance information needed to support the API
+    def create_table(self,
+                     table_name: str,
+                     fields: List[DBField],
+                     key_field_name: str) -> DBTable:
+        s = shelve.open(f'{table_name}.db', writeback=True)
+        try:
+            s[table_name] = dict
+        finally:
+            s.close()
+        return DBTable(table_name, fields, key_field_name)
+
+    def num_tables(self) -> int:
+        raise NotImplementedError
+
+    def get_table(self, table_name: str) -> DBTable:
+        raise NotImplementedError
+
+    def delete_table(self, table_name: str) -> None:
+        raise NotImplementedError
+
+    def get_tables_names(self) -> List[Any]:
+        raise NotImplementedError
+
+    def query_multiple_tables(
+            self,
+            tables: List[str],
+            fields_and_values_list: List[List[SelectionCriteria]],
+            fields_to_join_by: List[str]
+    ) -> List[Dict[str, Any]]:
+        raise NotImplementedError
